2137183e639d0cacc0bd846578389a96cd8cceb5
BakaAfk <65333506+BakaAfk@users.noreply.github.com>
2024-01-08 20:58:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const observer1 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show1");
+    } else {
+      entry.target.classList.remove("show1");
+    }
+  });
+});
+
+const hiddenElements1 = document.querySelectorAll(".column1");
+hiddenElements1.forEach((el) => observer1.observe(el));
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show2");
+    } else {
+      entry.target.classList.remove("show2");
+    }
+  });
+});
+
+const hiddenElements2 = document.querySelectorAll(".column2");
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("zoom");
+    } 
+  });
+});
+
+const hiddenElements3 = document.querySelectorAll(".opening");
+hiddenElements3.forEach((el) => observer3.observe(el));
+
+history.scrollRestoration = "manual";
+
+$(window).on('beforeunload', function(){
+      $(window).scrollTop(0);
+});
